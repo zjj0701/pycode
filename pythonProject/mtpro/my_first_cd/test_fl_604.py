964301aaf1a3b8584dd5e4309f51bbd4b12a6cc7
zjj <544562474@qq.com>
2024-10-13 22:39:33 +0800

    神经网络的梯度

--- pythonProject/mtpro/my_first_cd/test_fl_604.py
+++ pythonProject/mtpro/my_first_cd/test_fl_604.py
@@ -1,5 +1,30 @@
-import sys, os
-# sys.path.append('../commons/')
 import numpy as np
-# sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-from commons import functions
+
+from pythonProject.mtpro.my_first_cd.commons.functions import softmax, cross_entropy_error
+
+
+class simpleNet:
+    def __init__(self):
+        self.W = np.random.randn(2, 3)  # 高斯分布初始化
+
+    def predict(self, X):
+        return np.dot(X, self.W)
+
+    def loss(self, x, t):
+        z = self.predict(x)
+        y = softmax(z)
+        loss = cross_entropy_error(y, t)
+
+        return loss
+
+
+if __name__ == '__main__':
+    net = simpleNet()
+    print(f"net's w is:{net.W}")
+    x = np.array([0.6, 0.9])
+    p = net.predict(x)
+    print(f"predict is:{p}")
+    print(f"the max index of p is:{np.argmax(p)}")
+
+    t = np.array([0, 0, 1])
+    print(f"the loss is:{net.loss(x, t)}")
