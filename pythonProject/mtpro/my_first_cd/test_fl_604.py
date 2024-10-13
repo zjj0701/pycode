6637929b1029f1fb0ade2f955b148be7d133d34b
zjj <544562474@qq.com>
2024-10-13 22:27:11 +0800

    code format

---
+++ pythonProject/mtpro/my_first_cd/test_fl_604.py
@@ -0,0 +1,5 @@
+import sys, os
+# sys.path.append('../commons/')
+import numpy as np
+# sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from commons import functions
