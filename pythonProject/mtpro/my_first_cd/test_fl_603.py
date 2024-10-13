6d6b27d45f5d5414b214300ac6dae7c403998a47
zjj <544562474@qq.com>
2024-10-13 21:59:45 +0800

    求解梯度format code

--- pythonProject/mtpro/my_first_cd/test_fl_603.py
+++ pythonProject/mtpro/my_first_cd/test_fl_603.py
@@ -33,14 +33,15 @@ def gradient_descent(f, int_x, lr=0.01, step_num=100):
 def function_2(x):
     return x[0] ** 2 + x[1] ** 2
 
+
 if __name__ == '__main__':
-    init_x = np.array([-3.0,4.0])
+    init_x = np.array([-3.0, 4.0])
     # 此时的学习率是0.1
     res = gradient_descent(function_2, init_x, lr=0.1, step_num=100)
     print(f"学习率是0.1：{res}")
-    #学习率过大为10.0
+    # 学习率过大为10.0
     res = gradient_descent(function_2, init_x, lr=10.0, step_num=100)
     print(f"学习率是10：{res}")
-    #学习率过小为1e-10
+    # 学习率过小为1e-10
     res = gradient_descent(function_2, init_x, lr=1e-10, step_num=100)
     print(f"学习率是1e-10：{res}")
