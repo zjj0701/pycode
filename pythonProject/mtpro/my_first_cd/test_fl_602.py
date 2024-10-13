ad18957ce3f1045c206fa5913b82d423f012c57c
zjj <544562474@qq.com>
2024-10-13 21:42:53 +0800

    计算梯度

---
+++ pythonProject/mtpro/my_first_cd/test_fl_602.py
@@ -0,0 +1,29 @@
+#
+import numpy as np
+
+
+# 初始函数
+def function_2(x):
+    return x[0] ** 2 + x[1] ** 2
+
+
+# 计算梯度
+def numerical_gradient(f, x):
+    h = 1e-4
+    grad = np.zeros_like(x)
+
+    for idx in range(x.size):
+        tmp_val = x[idx]
+        x[idx] = tmp_val + h
+        fxh1 = f(x)
+
+        x[idx] = tmp_val - h
+        fxh2 = f(x)
+
+        grad[idx] = (fxh1 - fxh2) / (2 * h)
+        x[idx] = tmp_val
+
+    return grad
+
+
+print(numerical_gradient(function_2, np.array([3.0, 4.0])))
