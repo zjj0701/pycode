210fe58cd236bacca58775ca919db1553cbdcd14
zjj <544562474@qq.com>
2024-10-15 18:06:09 +0800

    注释

---
+++ pythonProject/mtpro/my_first_cd/test.py
@@ -0,0 +1,5 @@
+import numpy as np
+
+list = 0.01 * np.random.randn(28 * 28, 50)
+# shape is 784 * 50
+print(list)
--- pythonProject/mtpro/my_first_cd/test_fl_203.py
+++ pythonProject/mtpro/my_first_cd/test_fl_203.py
@@ -9,7 +9,7 @@ from pythonProject.mtpro.my_first_cd.commons.layers import Affine, Relu, Softmax
 class TwoLayerNet:
 
     def __init__(self, input_size, hidden_size, output_size, weight_init_std=0.01):
-        # 初始化权重
+        # 初始化权重--高斯分布初始化
         self.params = {}
         self.params['W1'] = weight_init_std * np.random.randn(input_size, hidden_size)
         self.params['b1'] = np.zeros(hidden_size)
@@ -18,10 +18,14 @@ class TwoLayerNet:
 
         # 生成层
         self.layers = OrderedDict()
+        # 第一层全连接层
         self.layers['Affine1'] = Affine(self.params['W1'], self.params['b1'])
+        # 第一层激活函数
         self.layers['Relu1'] = Relu()
+        # 第二层全连接层
         self.layers['Affine2'] = Affine(self.params['W2'], self.params['b2'])
 
+        # 最后一层
         self.lastLayer = SoftmaxWithLoss()
 
     def predict(self, x):
@@ -74,4 +78,3 @@ class TwoLayerNet:
         grads['W2'], grads['b2'] = self.layers['Affine2'].dW, self.layers['Affine2'].db
 
         return grads
-
