1ffb3b2f04ad03be6fbc536b14532579a86dbe19
zjj <544562474@qq.com>
2024-10-18 22:48:29 +0800

    format:code

--- pythonProject/deepstudy/test01.py
+++ pythonProject/deepstudy/test01.py
@@ -1,27 +1,32 @@
 from torch.utils.data import Dataset, DataLoader
 from PIL import Image
 import os
+
+
 class MyDataset(Dataset):
 
-    def __init__(self,root_dir,label_dir):
+    def __init__(self, root_dir, label_dir):
         self.root_dir = root_dir
         self.label_dir = label_dir
-        self.path = os.path.join(self.root_dir,self.label_dir)
+        self.path = os.path.join(self.root_dir, self.label_dir)
         self.img_path = os.listdir(self.path)
+
     def __getitem__(self, index):
         img_name = self.img_path[index]
-        img_item_path = os.path.join(self.root_dir,self.label_dir,img_name)
+        img_item_path = os.path.join(self.root_dir, self.label_dir, img_name)
         img = Image.open(img_item_path)
         label = self.label_dir
         return img, label
+
     def __len__(self):
         return len(self.img_path)
 
+
 root_dir = "dataset/train/"
 ants_label_dir = "ant"
-ants_dataset = MyDataset(root_dir,ants_label_dir)
+ants_dataset = MyDataset(root_dir, ants_label_dir)
 bees_label_dir = "bees"
-bees_dataset = MyDataset(root_dir,bees_label_dir)
+bees_dataset = MyDataset(root_dir, bees_label_dir)
 
-train_data = ants_dataset+bees_dataset
+train_data = ants_dataset + bees_dataset
 print(train_data)
--- pythonProject/deepstudy/test_tb.py
+++ pythonProject/deepstudy/test_tb.py
@@ -7,8 +7,8 @@ writer = SummaryWriter("logs")
 img_path = 'dataset/train/ant/0013035.jpg'
 img = Image.open(img_path)
 img_arr = np.array(img)
-print(img_arr.shape) #形状是(512, 768, 3) H W　C　通道在最后
+print(img_arr.shape)  # 形状是(512, 768, 3) H W　C　通道在最后
 writer.add_image("test", img_arr, 1, dataformats="HWC")
 for i in range(100):
-    writer.add_scalar("y = x",i,i)
+    writer.add_scalar("y = x", i, i)
 writer.close()
