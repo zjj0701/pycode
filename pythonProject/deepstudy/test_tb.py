b4e5a43be07e5644e6487a76c0d427d59b0c6cc7
zjj <544562474@qq.com>
2024-10-18 23:04:32 +0800

    format:code

--- pythonProject/deepstudy/test_tb.py
+++ pythonProject/deepstudy/test_tb.py
@@ -4,7 +4,7 @@ from PIL import Image
 
 writer = SummaryWriter("logs")
 
-img_path = 'dataset/train/ant/0013035.jpg'
+img_path = 'dataset/train/ant/5650366_e22b7e1065.jpg'
 img = Image.open(img_path)
 img_arr = np.array(img)
 print(img_arr.shape)  # 形状是(512, 768, 3) H W　C　通道在最后
