97ab4cc240b0370bc97de72a4174dc4877380681
zjj <544562474@qq.com>
2024-10-22 17:27:49 +0800

    池化

--- pythonProject/deepstudy/nn_maxpool.py
+++ pythonProject/deepstudy/nn_maxpool.py
@@ -12,26 +12,22 @@ dataloader = DataLoader(dataset, batch_size=64)
 class Tudui(nn.Module):
     def __init__(self):
         super(Tudui, self).__init__()
-        self.conv1 = nn.Conv2d(3, 6, 3, stride=1, padding=0)
+        self.maxpool1 = nn.MaxPool2d(3, ceil_mode=False)
 
     def forward(self, x):
-        x = self.conv1(x)
+        x = self.maxpool1(x)
         return x
 
 
-writer = SummaryWriter("./logs")
+writer = SummaryWriter("./logs/maxpool")
 tudui = Tudui()
 step = 0
 for data in dataloader:
     img, label = data
     out = tudui(img)
     print(img.shape)  # [64, 3, 32, 32]
-    # [64, 6, 30, 30]
-    #img = img[0]  # 解法一：这里注意 add_image 方法期望输入张量的形状为 (C, H, W)
-    writer.add_image("input", img, step,dataformats="NCHW")
-    out = torch.reshape(out, (-1, 3, 30, 30))
-    print(out.shape)
-    out = out[0]
-    writer.add_image("output", out, step)
+    # 解法一：这里注意 add_image 方法期望输入张量的形状为 (C, H, W)
+    writer.add_image("input", img, step, dataformats="NCHW")
+    writer.add_image("output", out, step, dataformats="NCHW")
     step += 1
 writer.close()
