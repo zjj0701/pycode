050a807861dee28d0892c3f77a6b7e46127ba413
zjj <544562474@qq.com>
2024-10-23 21:34:36 +0800

    fix:预训练

--- pythonProject/deepstudy/nn_pretrain.py
+++ pythonProject/deepstudy/nn_pretrain.py
@@ -11,4 +11,4 @@ vgg_16_true.add_module("add_Linear", nn.Linear(1000, 10))
 print(f"after:{vgg_16_true}")
 # 怎么在classfier里面加或者修改？
 vgg_16_false.classifier[7] = nn.Linear(1000,10)
-print(f"classifier after :{vgg_16_true}")
+print(f"classifier after :{vgg_16_false}")
